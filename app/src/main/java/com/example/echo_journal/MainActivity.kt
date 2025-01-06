a5cc08b54516dd022e1fa769c28a464612ce7fd3
arturgie <gubala-artur@wp.pl>
2025-01-06 18:34:07 +0100

    Fix splash screen configuration. Import splash screen icon based on figma mockups. Set up application icon.

--- app/src/main/java/com/example/echo_journal/MainActivity.kt
+++ app/src/main/java/com/example/echo_journal/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.echo_journal.ui.theme.EchojournalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             EchojournalTheme {
