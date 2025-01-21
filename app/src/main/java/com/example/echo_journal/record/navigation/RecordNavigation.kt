cb95e78e81166c00441bd978b8e196f805baa238
arturgie <gubala-artur@wp.pl>
2025-01-21 22:50:38 +0100

    Implement navigation for record and settings using type safe compose navigation.

--- app/src/main/java/com/example/echo_journal/record/navigation/RecordNavigation.kt
+++ app/src/main/java/com/example/echo_journal/record/navigation/RecordNavigation.kt
@@ -1,3 +1,30 @@
 package com.example.echo_journal.record.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
 
+fun NavController.navigateToRecordHistory(navOptions: NavOptions? = null) = navigate(RecordHistoryScreen, navOptions)
+fun NavController.navigateToCreateRecord(path: String, navOptions: NavOptions? = null) {
+    navigate(CreateRecordScreen(path = path), navOptions)
+}
+
+fun NavGraphBuilder.recordHistoryScreen() {
+    composable<RecordHistoryScreen> {}
+}
+
+fun NavGraphBuilder.createRecordScreen(
+        onBackClick: () -> Unit
+) {
+    composable<CreateRecordScreen> {}
+}
+
+@Serializable
+object RecordHistoryScreen
+
+@Serializable
+data class CreateRecordScreen(
+    val path: String
+)
--- app/src/main/java/com/example/echo_journal/settings/navigation/SettingsNavigation.kt
+++ app/src/main/java/com/example/echo_journal/settings/navigation/SettingsNavigation.kt
@@ -1,2 +1,16 @@
 package com.example.echo_journal.settings.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToSettings(navOptions: NavOptions? = null) = navigate(SettingsScreen, navOptions)
+
+fun NavGraphBuilder.settingsScreen() {
+    composable<SettingsScreen> {}
+}
+
+@Serializable
+object SettingsScreen
