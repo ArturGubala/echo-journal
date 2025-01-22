f9fb6a029a648fa77524da4f8d68109f2c1e1639
arturgie <gubala-artur@wp.pl>
2025-01-22 23:14:06 +0100

    Create NavHost for Echo Journal.

--- app/src/main/java/com/example/echo_journal/core/presentation/navigation/EchoJournalNavHost.kt
+++ app/src/main/java/com/example/echo_journal/core/presentation/navigation/EchoJournalNavHost.kt
@@ -1,2 +1,25 @@
 package com.example.echo_journal.core.presentation.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.echo_journal.record.navigation.RecordHistoryScreen
+import com.example.echo_journal.record.navigation.recordHistoryScreen
+import com.example.echo_journal.settings.navigation.settingsScreen
+
+@Composable
+fun EchoJournalNavHost(
+    modifier: Modifier = Modifier,
+    startDestination: Any = RecordHistoryScreen
+) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        modifier = modifier,
+        startDestination = startDestination
+    ) {
+        recordHistoryScreen()
+        settingsScreen()
+    }
+}
