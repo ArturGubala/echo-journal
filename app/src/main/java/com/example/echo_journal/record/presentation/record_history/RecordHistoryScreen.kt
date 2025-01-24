b70f79aca427ada8a7abfdd47bf1047e62c59ae8
arturgie <gubala-artur@wp.pl>
2025-01-24 23:30:44 +0100

    Add floating button for record history screen.

--- app/src/main/java/com/example/echo_journal/record/presentation/record_history/RecordHistoryScreen.kt
+++ app/src/main/java/com/example/echo_journal/record/presentation/record_history/RecordHistoryScreen.kt
@@ -5,7 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,6 +19,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.echo_journal.R
@@ -31,14 +37,24 @@ private fun RecordHistoryScreen() {
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    containerColor = Color.Transparent,
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
                     Text("Small Top App Bar")
                 }
             )
-        }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(Icons.Filled.Add, "Floating action button.")
+            }
+        },
+        floatingActionButtonPosition = androidx.compose.material3.FabPosition.End
     ) { padding ->
         Column(
             modifier = Modifier
