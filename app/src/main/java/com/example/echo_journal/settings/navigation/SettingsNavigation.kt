4edab2d4c4a808a06e319939da1af613459a2c66
arturgie <gubala-artur@wp.pl>
2025-01-27 20:59:44 +0100

    Implement base view for settings screen and call it from SettingsNavigation.

--- app/src/main/java/com/example/echo_journal/settings/navigation/SettingsNavigation.kt
+++ app/src/main/java/com/example/echo_journal/settings/navigation/SettingsNavigation.kt
@@ -4,12 +4,15 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.example.echo_journal.settings.presentation.SettingsRoute
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToSettings(navOptions: NavOptions? = null) = navigate(SettingsScreen, navOptions)
 
 fun NavGraphBuilder.settingsScreen() {
-    composable<SettingsScreen> {}
+    composable<SettingsScreen> {
+        SettingsRoute()
+    }
 }
 
 @Serializable
--- app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
@@ -1,2 +1,40 @@
 package com.example.echo_journal.settings.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun SettingsRoute() {
+    SettingsScreen()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SettingsScreen() {
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier.padding(padding)
+        ) {
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                )
+            ) {
+                Text(
+                    text = "Elevated",
+                    modifier = Modifier
+                        .padding(16.dp),
+                )
+            }
+
+        }
+    }
+}
