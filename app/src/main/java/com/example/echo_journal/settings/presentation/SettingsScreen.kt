86a6edf1c75ddaed16b827bb8984e71f6ad8ed6e
arturgie <gubala-artur@wp.pl>
2025-01-28 22:25:15 +0100

    Add base settings card implementation.

--- app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
@@ -1,15 +1,30 @@
 package com.example.echo_journal.settings.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.echo_journal.R
 
 @Composable
 internal fun SettingsRoute() {
@@ -19,20 +34,58 @@ internal fun SettingsRoute() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun SettingsScreen() {
-    Scaffold { padding ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Settings")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+
+            )
+        },
+    ) { padding ->
         Column(
-            modifier = Modifier.padding(padding)
+            modifier = Modifier
+                .padding(padding)
+                .padding(top = 10.dp)
+                .padding(horizontal = 10.dp)
         ) {
             ElevatedCard(
                 elevation = CardDefaults.cardElevation(
-                    defaultElevation = 6.dp
-                )
+                    defaultElevation = 6.dp,
+                ),
+                modifier = Modifier
+                    .fillMaxWidth(),
             ) {
                 Text(
-                    text = "Elevated",
+                    text = "My Mood",
                     modifier = Modifier
                         .padding(16.dp),
                 )
+                Text(
+                    text = "Select default mood to apply to all new entries",
+                    modifier = Modifier
+                        .padding(16.dp),
+                )
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.ic_stressed),
+                        contentDescription = "content description",
+                        colorFilter = ColorFilter.tint(Color.Red, BlendMode.SrcIn)
+                    )
+                }
             }
 
         }
