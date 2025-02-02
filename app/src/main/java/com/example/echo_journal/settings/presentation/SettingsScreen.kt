2364308a7cadac75dff6b47c507dbafdbbb68187
arturgie <gubala-artur@wp.pl>
2025-02-02 22:57:10 +0100

    Add created card component to settings screen.

--- app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
@@ -1,15 +1,13 @@
 package com.example.echo_journal.settings.presentation
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,13 +16,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.echo_journal.R
+import com.example.echo_journal.settings.presentation.components.CardHeader
+import com.example.echo_journal.settings.presentation.components.IconWithText
+import com.example.echo_journal.settings.presentation.components.SettingsCard
 
 @Composable
 internal fun SettingsRoute() {
@@ -60,34 +59,60 @@ private fun SettingsScreen() {
             modifier = Modifier
                 .padding(padding)
                 .padding(top = 10.dp)
-                .padding(horizontal = 10.dp)
+                .padding(horizontal = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(14.dp)
         ) {
-            ElevatedCard(
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 6.dp,
-                ),
-                modifier = Modifier
-                    .fillMaxWidth(),
-            ) {
-                Text(
-                    text = "My Mood",
-                    modifier = Modifier
-                        .padding(16.dp),
-                )
-                Text(
-                    text = "Select default mood to apply to all new entries",
-                    modifier = Modifier
-                        .padding(16.dp),
-                )
-                Row {
-                    Image(
-                        painter = painterResource(R.drawable.ic_stressed),
-                        contentDescription = "content description",
-                        colorFilter = ColorFilter.tint(Color.Red, BlendMode.SrcIn)
+            SettingsCard(
+                header =  {
+                    CardHeader(
+                        title = "My mood",
+                        subtitle = "Select default mood to apply to all new entries"
                     )
+                },
+                content = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconWithText(
+                            id = R.drawable.ic_stressed,
+                            text = "Stressed",
+                            modifier = Modifier.weight(1f)
+                        )
+                        IconWithText(
+                            id = R.drawable.ic_sad,
+                            text = "Sad",
+                            modifier = Modifier.weight(1f)
+                        )
+                        IconWithText(
+                            id = R.drawable.ic_neutral,
+                            text = "Neutral",
+                            modifier = Modifier.weight(1f)
+                        )
+                        IconWithText(
+                            id = R.drawable.ic_peaceful,
+                            text = "Peaceful",
+                            modifier = Modifier.weight(1f)
+                        )
+                        IconWithText(
+                            id = R.drawable.ic_excited,
+                            text = "Excited",
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
                 }
-            }
-
+            )
+            SettingsCard(
+                header =  {
+                    CardHeader(
+                        title = "My Topics",
+                        subtitle = "Select default topics to apply to all new entries"
+                    )
+                },
+                content = {}
+            )
         }
     }
 }
