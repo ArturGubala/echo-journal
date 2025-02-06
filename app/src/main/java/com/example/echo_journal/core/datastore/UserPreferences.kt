768735bc894b5b14ba6a66f8cf4c9a153f243902
arturgie <gubala-artur@wp.pl>
2025-02-06 23:42:20 +0100

    Change type of field defaultMood in UserPreferences from string to Mood enum.

--- app/src/main/java/com/example/echo_journal/core/datastore/UserPreferences.kt
+++ app/src/main/java/com/example/echo_journal/core/datastore/UserPreferences.kt
@@ -1,9 +1,10 @@
 package com.example.echo_journal.core.datastore
 
+import com.example.echo_journal.core.domain.Mood
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserPreferences(
-    val defaultMoodResId: String = "",
+    val defaultMood: Mood? = null,
     val defaultTopics: List<String> = emptyList()
 )
