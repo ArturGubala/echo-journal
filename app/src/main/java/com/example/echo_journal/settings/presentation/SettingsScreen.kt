b981eda03c371fed17567004c73a118e4d7f07cf
arturgie <gubala-artur@wp.pl>
2025-02-15 22:33:12 +0100

    Try to implement ExposedDropdownMenu.

--- app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/example/echo_journal/settings/presentation/SettingsScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -23,17 +22,19 @@ import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,6 +42,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -151,6 +154,8 @@ private fun SettingsScreen(
                     modifier = Modifier
                         .background(Color.White)
                 )
+                var expanded by remember { mutableStateOf(false) }
+//                val focusRequester = remember { FocusRequester() }
                 SettingsCard(
                     header = {
                         CardHeader(
@@ -160,99 +165,129 @@ private fun SettingsScreen(
                         )
                     },
                     content = {
-                        var expanded by remember { mutableStateOf(false) }
-                        FlowRow(
-                            horizontalArrangement = Arrangement.spacedBy(2.dp),
-                            verticalArrangement = Arrangement.aligned(Alignment.CenterVertically),
-                            modifier = Modifier.wrapContentSize()
+
+                        ExposedDropdownMenuBox(
+                            expanded = expanded,
+                            onExpandedChange = { expanded = !expanded }
                         ) {
-                            for (i in 0..3){
-                            AssistChip(
-                                label = {
-                                    Row(
-                                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                                        verticalAlignment = Alignment.CenterVertically
+
+                            FlowRow(
+                                horizontalArrangement = Arrangement.spacedBy(2.dp),
+                                verticalArrangement = Arrangement.aligned(Alignment.CenterVertically)
+                            ) {
+                                for (i in 0..3) {
+                                    AssistChip(
+                                        label = {
+                                            Row(
+                                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                                verticalAlignment = Alignment.CenterVertically
+                                            ) {
+                                                Text(
+                                                    text = "#",
+                                                    modifier = Modifier.padding(end = 3.dp)
+                                                        .alpha(0.5f)
+                                                )
+                                                Text(text = "Topic new en ${i + 1}")
+                                                Icon(
+                                                    imageVector = Icons.Filled.Clear,
+                                                    contentDescription = "Clear",
+                                                    modifier = Modifier
+                                                        .size(16.dp)
+                                                        .alpha(.3f)
+                                                        .clickable { }
+                                                )
+                                            }
+                                        },
+                                        modifier = Modifier,
+                                        onClick = { },
+                                        border = null,
+                                        shape = RoundedCornerShape(20.dp),
+                                        colors = AssistChipDefaults.assistChipColors(
+                                            labelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            containerColor = TopicBackground
+                                        )
+                                    )
+                                }
+                                val focusRequester = remember { FocusRequester() }
+
+                                if (!expanded) {
+                                    IconButton(
+                                        onClick = {
+//                                    onAction(SettingsAction.OnNewTopicClick)
+                                            expanded = !expanded
+                                        },
+                                        modifier = Modifier
+                                            .size(32.dp)
+//                                    .offset(x = 3.dp),
+                                            .align(alignment = Alignment.CenterVertically),
+                                        colors = IconButtonColors(
+                                            containerColor = TopicBackground,
+                                            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                                            disabledContentColor = MaterialTheme.colorScheme.primary
+
+                                        )
                                     ) {
-                                        Text(text = "#", modifier = Modifier.padding(end = 3.dp).alpha(0.5f))
-                                        Text(text = "Topic new en ${i+1}")
                                         Icon(
-                                            imageVector = Icons.Filled.Clear,
-                                            contentDescription = "Clear",
+                                            imageVector = Icons.Filled.Add,
+                                            contentDescription = "Add",
                                             modifier = Modifier
-                                                .size(16.dp)
-                                                .alpha(.3f)
-                                                .clickable { }
+                                                .size(24.dp)
                                         )
                                     }
-                                },
-                                modifier = Modifier,
-                                onClick = { },
-                                border = null,
-                                shape = RoundedCornerShape(20.dp),
-                                colors = AssistChipDefaults.assistChipColors(
-                                    labelColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                    leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                    containerColor = TopicBackground
-                                )
-                            )}
-                            IconButton(
-                                onClick = {
-                                    onAction(SettingsAction.OnNewTopicClick)
-                                          expanded = !expanded
-                                },
-                                modifier = Modifier
-                                    .size(32.dp)
-//                                    .offset(x = 3.dp),
-                                    .align(alignment = Alignment.CenterVertically),
-                                colors = IconButtonColors(
-                                    containerColor = TopicBackground,
-                                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                    disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                                    disabledContentColor = MaterialTheme.colorScheme.primary
+                                } else {
 
-                                )
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Filled.Add,
-                                    contentDescription = "Add",
-                                    modifier = Modifier
-                                        .size(24.dp)
-                                )
-                            }
-                            BasicTextField(
-                                value = state.newTopic,
-                                onValueChange = { onAction(SettingsAction.OnTopicTextChange(it)) },
-                                modifier = Modifier
-                                    .weight(1f)
-                                    .align(Alignment.CenterVertically),
-                                decorationBox = { innerTextField ->
-                                    Box(
-                                        contentAlignment = Alignment.CenterStart,
-                                    ) {
-                                        innerTextField()
-                                        if (state.newTopic.isEmpty()) {
-                                            Text(
-                                                text = stringResource(R.string.default_topic_settings_placeholder),
-                                                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                                                modifier = Modifier.alpha(0.5f).padding(start = 2.dp)
+
+                                    BasicTextField(
+                                        value = state.newTopic,
+                                        onValueChange = {
+                                            onAction(
+                                                SettingsAction.OnTopicTextChange(
+                                                    it
+                                                )
                                             )
+                                        },
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .align(Alignment.CenterVertically)
+                                            .menuAnchor(MenuAnchorType.PrimaryEditable)
+                                            .focusRequester(focusRequester),
+                                        decorationBox = { innerTextField ->
+                                            Box(
+                                                contentAlignment = Alignment.CenterStart,
+                                            ) {
+                                                innerTextField()
+                                                if (state.newTopic.isEmpty()) {
+                                                    Text(
+                                                        text = stringResource(R.string.default_topic_settings_placeholder),
+                                                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                                        modifier = Modifier.alpha(0.5f)
+                                                            .padding(start = 2.dp)
+                                                    )
+                                                }
+                                            }
                                         }
+                                    )
+
+                                    LaunchedEffect(Unit) {
+                                            focusRequester.requestFocus()
+                                    }
+
+                                    ExposedDropdownMenu(
+                                        expanded = expanded,
+                                        onDismissRequest = { expanded = false }
+                                    ) {
+                                        DropdownMenuItem(
+                                            text = { Text("Option 1") },
+                                            onClick = { expanded = false },
+                                        )
+                                        DropdownMenuItem(
+                                            text = { Text("Option 2") },
+                                            onClick = { expanded = false }
+                                        )
                                     }
                                 }
-                            )
-                            DropdownMenu(
-                                expanded = expanded,
-                                onDismissRequest = { },
-                                modifier = Modifier.fillMaxWidth(0.87f)
-                            ) {
-                                DropdownMenuItem(
-                                    text = { Text("Option 1") },
-                                    onClick = { /* Do something... */ }
-                                )
-                                DropdownMenuItem(
-                                    text = { Text("Option 2") },
-                                    onClick = { /* Do something... */ }
-                                )
                             }
                         }
                     },
