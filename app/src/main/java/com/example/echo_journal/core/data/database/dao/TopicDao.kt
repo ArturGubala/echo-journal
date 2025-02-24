e9772d6a08db13ae663a1f458f31b869a9e60ccb
arturgie <gubala-artur@wp.pl>
2025-02-24 21:00:47 +0100

    Rename TopicDb to TopicEntity for clarity. Add functions to TopicDao interface.

--- app/src/main/java/com/example/echo_journal/core/data/database/dao/TopicDao.kt
+++ app/src/main/java/com/example/echo_journal/core/data/database/dao/TopicDao.kt
@@ -1,7 +1,27 @@
 package com.example.echo_journal.core.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.echo_journal.core.data.database.entity.TopicEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TopicDao {
+
+    @Query("SELECT * FROM topics")
+    fun getTopics(): Flow<List<TopicEntity>>
+
+    @Query("SELECT * FROM topics WHERE id IN (:ids) ORDER BY name ASC")
+    suspend fun getTopicsByIds(ids: List<Long>): List<TopicEntity>
+
+    @Query("SELECT * FROM topics WHERE name LIKE '%' || :query || '%' ORDER BY name ASC")
+    suspend fun searchTopics(query: String): List<TopicEntity>
+
+    @Insert
+    suspend fun insertTopic(topic: TopicEntity)
+
+    @Delete
+    suspend fun deleteTopic(topic: TopicEntity)
 }
--- app/src/main/java/com/example/echo_journal/core/data/database/entity/TopicEntity.kt
+++ app/src/main/java/com/example/echo_journal/core/data/database/entity/TopicEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "topics")
-data class TopicDb(
+data class TopicEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
     val name: String
