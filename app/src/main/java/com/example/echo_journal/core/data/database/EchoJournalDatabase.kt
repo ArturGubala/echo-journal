7cdfd432b2c808d71d35a01ef28aad6d428a1ada
arturgie <gubala-artur@wp.pl>
2025-02-28 22:35:12 +0100

    Delete topic related db elements, because of implements topics with datastore. Leave empty db class for future record elements.

--- app/src/main/java/com/example/echo_journal/core/data/database/EchoJournalDatabase.kt
+++ app/src/main/java/com/example/echo_journal/core/data/database/EchoJournalDatabase.kt
@@ -2,15 +2,11 @@ package com.example.echo_journal.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.echo_journal.core.data.database.dao.TopicDao
-import com.example.echo_journal.core.data.database.entity.TopicEntity
 
 @Database(
-    entities = [TopicEntity::class],
+    entities = [],
     version = 1,
     exportSchema = false
 )
 
-abstract class EchoJournalDatabase : RoomDatabase() {
-    abstract fun topicDao(): TopicDao
-}
+abstract class EchoJournalDatabase : RoomDatabase()
--- app/src/main/java/com/example/echo_journal/core/data/database/di/DatabaseModule.kt
+++ app/src/main/java/com/example/echo_journal/core/data/database/di/DatabaseModule.kt
@@ -2,11 +2,7 @@ package com.example.echo_journal.core.data.database.di
 
 import androidx.room.Room
 import com.example.echo_journal.core.data.database.EchoJournalDatabase
-import com.example.echo_journal.core.data.database.RoomLocalTopicDataSource
-import com.example.echo_journal.core.domain.topic.LocalTopicDataSource
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val databaseModule = module {
@@ -17,6 +13,4 @@ val databaseModule = module {
             "echo_journal_db"
         ).build()
     }
-    single { get<EchoJournalDatabase>().topicDao() }
-    singleOf(::RoomLocalTopicDataSource).bind<LocalTopicDataSource>()
 }
