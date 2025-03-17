2566d79e9f4ea27b20bd334648c8b7c9c243e0f7
arturgie <gubala-artur@wp.pl>
2025-03-17 21:40:31 +0100

    Create RecordCreateState class.

---
+++ app/src/main/java/com/example/echo_journal/record/presentation/record_create/RecordCreateState.kt
@@ -0,0 +1,28 @@
+package com.example.echo_journal.record.presentation.record_create
+
+import androidx.compose.runtime.Stable
+import com.example.echo_journal.core.domain.MoodUi
+import com.example.echo_journal.core.domain.PlayerState
+import com.example.echo_journal.core.domain.topic.Topic
+
+class RecordCreateState(
+    val currentMood: MoodUi,
+    val titleValue: String = "",
+    val topicValue: String = "",
+    val descriptionValue: String = "",
+    val playerState: PlayerState = PlayerState(),
+    val currentTopics: List<Topic> = emptyList(),
+    val foundTopics: List<Topic> = emptyList(),
+    val recordSheetState: RecordSheetState = RecordSheetState(),
+    val showLeaveDialog: Boolean = false
+) {
+    val isSaveButtonEnabled: Boolean
+        get() = titleValue.isNotBlank()
+
+    @Stable
+    data class RecordSheetState(
+        val isOpened: Boolean = false,
+        val activeMood: MoodUi? = null,
+        val moods: List<MoodUi> = emptyList()
+    )
+}
